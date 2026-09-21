9cd53096f825d5330dce8f4bf582c8dc9fd24152
Chinto-lgtm <ahmadsaith145@gmail.com>
2026-09-22 02:05:35 +0500

    Track the docs theme; only ignore the root VitePress cache

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,6 @@
+import DefaultTheme from 'vitepress/theme'
+// The docs read the same theme file as the app: src/theme.css is the single source of truth.
+import '../../../src/theme.css'
+import './custom.css'
+
+export default DefaultTheme
